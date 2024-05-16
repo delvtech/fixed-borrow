4aa91395d2769799b0a41bac0c1cac31d7beaafc
Cash Baller <cashd@users.noreply.github.com>
2024-05-16 15:45:09 -0500

    Scaffolding market borrow card

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query"
+import { Button } from "components/ui/button"
 import {
   Card,
   CardContent,
@@ -6,6 +7,7 @@ import {
   CardHeader,
   CardTitle,
 } from "components/ui/card"
+import { Separator } from "components/ui/separator"
 import { MorphoMarketReader } from "lib/markets/MarketsReader"
 import { Address } from "viem"
 
@@ -26,16 +28,84 @@ interface MarketCardProps {
 
 function MarketCard(props: MarketCardProps) {
   return (
-    <Card>
+    <Card className="min-w-[1200px]">
       <CardHeader>
         <CardTitle>{props.marketTitle}</CardTitle>
         <CardDescription>{props.marketDescription}</CardDescription>
       </CardHeader>
 
-      <CardContent>
-        <div className="flex flex-col">
-          <span className="font-semibold font-display">Total Col.</span>
-          <span>100,000</span>
+      <CardContent className="flex flex-row gap-x-12">
+        <div className="grid grid-cols-2 grid-rows-2 gap-4 grow grid-col">
+          <div className="flex gap-x-2 items-start">
+            <span className="text-sm text-gray-600">Total Col.</span>
+            <div>
+              <div className="font-medium font-display leading-5 text-lg">
+                890.3 wstETH
+              </div>
+              <div className="text-sm text-gray-600">$122,000</div>
+            </div>
+          </div>
+
+          <div className="flex gap-x-2 items-start">
+            <span className="text-sm text-gray-600">Debt</span>
+            <div>
+              <div className="font-medium font-display leading-5 text-lg">
+                50,000 USDC
+              </div>
+              <div className="text-sm text-gray-600">$170,000</div>
+            </div>
+          </div>
+
+          <div className="flex gap-x-2 items-start">
+            <span className="text-sm text-gray-600">Liq. Price</span>
+            <div>
+              <div className="font-medium font-display leading-5 text-lg">
+                32%
+              </div>
+              <div className="text-sm text-gray-600">86% Max LTV</div>
+            </div>
+          </div>
+
+          <div className="flex gap-x-2 items-start">
+            <span className="text-sm text-gray-600">LTV</span>
+            <div>
+              <div className="font-medium font-display leading-5 text-lg">
+                32%
+              </div>
+              <div className="text-sm text-gray-600">86% Max LTV</div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <Separator className="min-h-full" orientation="vertical" />
+        </div>
+
+        <div className="grid grid-cols-1 grid-rows-2 gap-4 grow">
+          <div className="flex flex-col">
+            <span className="text-sm text-gray-600">Current Borrow APY</span>
+            <div className="font-medium font-display leading-5 text-lg">
+              9.20%
+            </div>
+            <div className="text-sm text-gray-600">+2.00% (7d)</div>
+          </div>
+
+          <div className="flex flex-col">
+            <span className="text-sm text-gray-600">30d avg Borrow APY</span>
+            <div className="font-medium font-display leading-5 text-lg">
+              12.31%
+            </div>
+            <div className="text-sm text-gray-600">+1.52% (7d)</div>
+          </div>
+        </div>
+
+        <div>
+          <Separator className="min-h-full" orientation="vertical" />
+        </div>
+
+        <div className="m-auto flex flex-col items-center gap-y-2 grow">
+          <div className="text-2xl font-semibold">10.41%</div>
+          <Button>Fix your rate</Button>
         </div>
       </CardContent>
     </Card>
@@ -48,15 +118,17 @@ export function DashboardPage() {
   )
 
   return (
-    <main className="h-40 w-full h-min-screen mt-8">
-      {borrowPositions.map((position) => {
-        return (
-          <MarketCard
-            marketTitle={position.market?.loanAsset?.symbol}
-            marketDescription={`${position.market.loanAsset.name}`}
-          />
-        )
-      })}
+    <main className="h-min-screen mt-16">
+      <div className="flex flex-col gap-y-12 items-center">
+        {borrowPositions.map((position) => {
+          return (
+            <MarketCard
+              marketTitle={position.market?.loanAsset?.symbol}
+              marketDescription={`${position.market.loanAsset.name}`}
+            />
+          )
+        })}
+      </div>
       {/* <Card>
         <CardHeader>
           <CardTitle>Your Positions</CardTitle>
