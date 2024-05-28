95c9733ea9e46713c364671a59e36a4834ec1441
Cash Baller <cashd@users.noreply.github.com>
2024-05-28 14:45:23 -0500

    Add button to nav

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,9 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit"
+import { Button } from "components/ui/button"
 
 export function Navbar() {
   return (
-    <div className="grid grid-cols-3 py-2 items-center">
+    <div className="grid grid-cols-3 py-2 items-center px-8 border-b-gray-100 border-b">
       <div>
         <span className="text-xl font-semibold font-chakra">
           Hyperdrive Borrow
@@ -11,7 +12,9 @@ export function Navbar() {
 
       <div></div>
 
-      <div className="justify-self-end">
+      <div className="justify-self-end flex gap-x-2">
+        <Button variant="secondary">Learn how fixed rates work</Button>
+
         <ConnectButton chainStatus="none" showBalance={false} />
       </div>
     </div>
