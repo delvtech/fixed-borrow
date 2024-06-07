134ef64ad98a3f7d66669b31484d93b24ecfa2f2
Cash Baller <cashd@users.noreply.github.com>
2024-06-07 15:18:18 -0500

    custom rpc

--- src/client/rainbowClient.ts
+++ src/client/rainbowClient.ts
@@ -1,11 +1,20 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit"
 import { mainnet, sepolia } from "viem/chains"
+import { http } from "wagmi"
 
 export const rainbowConfig = getDefaultConfig({
   appName: "Hyperdrive Borrow",
   projectId: import.meta.env.VITE_WALLET_CONNECT_ID,
   chains: [mainnet, sepolia],
   ssr: false, // If your dApp uses server side rendering (SSR)
+  transports: {
+    [mainnet.id]: http(
+      "https://eth-mainnet.g.alchemy.com/v2/jhjiuDykxwKqhI8hEbj15nV2ZKED7O6z"
+    ),
+    [sepolia.id]: http(
+      "https://eth-sepolia.g.alchemy.com/v2/1lwuV3-H1ieTJ_tXRFJz2s5cXpmtJTvD"
+    ),
+  },
 })
 
 export const viemClient = rainbowConfig.getClient()
