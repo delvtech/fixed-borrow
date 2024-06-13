8b8bcbb7bbed2640ea1e9c11ba8afc8d78baf059
Cash Baller <cashd@users.noreply.github.com>
2024-06-13 17:45:19 -0500

    default badge and destruction variant

--- src/components/base/badge.tsx
+++ src/components/base/badge.tsx
@@ -4,16 +4,16 @@ import * as React from "react"
 import { cn } from "components/utils"
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  "inline-flex items-center rounded-[8px] border py-[4px] px-[8px] text-sm transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 cursor-default",
   {
     variants: {
       variant: {
         default:
-          "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
+          "border-transparent bg-secondary text-foreground border-border-secondary",
         secondary:
           "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
         destructive:
-          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
+          "border-transparent bg-destructive/25 text-destructive-foreground hover:bg-destructive/10",
         outline: "text-foreground",
       },
     },
