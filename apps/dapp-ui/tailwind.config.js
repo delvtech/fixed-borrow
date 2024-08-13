0dc01bbca696391aff0a6dbb08ed38a77b872158
Cash Baller <cashd@users.noreply.github.com>
2024-08-12 21:59:45 -0500

    Update tailwind.config.js

--- apps/dapp-ui/tailwind.config.js
+++ apps/dapp-ui/tailwind.config.js
@@ -39,6 +39,20 @@ module.exports = {
       h5: ["20px", { lineHeight: "28px" }],
     },
     extend: {
+      keyframes: {
+        "collapsible-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-collapsible-content-height)" },
+        },
+        "collapsible-up": {
+          from: { height: "var(--radix-collapsible-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "collapsible-down": "collapsible-down 0.2s ease-out",
+        "collapsible-up": "collapsible-up 0.2s ease-out",
+      },
       colors: {
         border: "hsl(var(--border))",
         "border-secondary": "hsl(var(--border-secondary))",
