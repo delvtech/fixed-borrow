1a8a3906b81338725e75528eb65d02173a78460a
Cash Baller <cashd@users.noreply.github.com>
2024-08-12 22:42:56 -0500

    fix ethereum config type

--- packages/config/src/static/index.ts
+++ packages/config/src/static/index.ts
@@ -1,6 +1,39 @@
-import sepoliaConfig from "./sepolia-config.json";
-import mainnetConfig from "./ethereum-config.json";
+import sepoliaConfigJson from "./sepolia-config.json";
+import mainnetConfigJson from "./ethereum-config.json";
 
-export type Config = typeof sepoliaConfig;
+export type Config = {
+  tokens: {
+    address: string;
+    symbol: string;
+    name: string;
+    decimals: number;
+    iconUrl: string;
+  }[];
+  morphoMarkets: {
+    id: string;
+    hyperdrive: string;
+    loanToken: {
+      address: string;
+      symbol: string;
+      name: string;
+      decimals: number;
+      iconUrl: string;
+    };
+    collateralToken: {
+      address: `0x${string}`;
+      symbol: string;
+      name: string;
+      decimals: number;
+      iconUrl: string;
+    };
+    oracle: `0x${string}`;
+    irm: `0x${string}`;
+    lltv: string;
+    termLength: string;
+  }[];
+};
+
+const sepoliaConfig = sepoliaConfigJson as Config;
+const mainnetConfig = mainnetConfigJson as Config;
 
 export { sepoliaConfig, mainnetConfig };
