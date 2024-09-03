25a13d86a5a148b9528c352228812735c5b9a33a
Ryan Goree <goree.ryan@gmail.com>
2024-09-03 18:13:25 -0500

    Fix separator import

--- apps/ui/src/components/base/separator.tsx
+++ apps/ui/src/components/base/separator.tsx
@@ -1,7 +1,7 @@
-import * as React from "react"
 import * as SeparatorPrimitive from "@radix-ui/react-separator"
+import * as React from "react"
 
-import { cn } from "s/lib/utils"
+import { cn } from "components/utils"
 
 const Separator = React.forwardRef<
   React.ElementRef<typeof SeparatorPrimitive.Root>,
