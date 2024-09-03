85b6dbd53ca4957c04330b6d6c4307585f044210
Ryan Goree <goree.ryan@gmail.com>
2024-09-03 16:10:25 -0500

    Replace hardcoded hex with tailwind class

--- apps/ui/src/components/base/button.tsx
+++ apps/ui/src/components/base/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary: "bg-[#2F333C] text-white hover:bg-[#2F333C]/80",
+        secondary: "bg-muted text-white hover:bg-muted/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
