62c53e3819dcfa89b913745bf54e9c6da5414a47
Cash Baller <cashd@users.noreply.github.com>
2024-09-11 21:01:27 -0500

    create position script working

--- apps/ui/src/utils/test/createTestPosition.ts
+++ apps/ui/src/utils/test/createTestPosition.ts
@@ -1,43 +1,49 @@
-import { fixed } from "@delvtech/fixed-point-wasm"
 import { useMutation } from "@tanstack/react-query"
-import { MorphoBlueAbi } from "lib/morpho/abi/MorphoBlueAbi"
 import { getAppConfig } from "utils/getAppConfig"
-import { erc20Abi, maxUint256, parseAbi } from "viem"
+import { useChainId, usePublicClient, useWalletClient } from "wagmi"
+import { morphoAddressesByChain, SupportedChainId } from "~/constants"
+
+import { MorphoBlueAbi } from "lib/morpho/abi/MorphoBlueAbi"
+import { testClient } from "src/client/rainbowClient"
 import {
-  useAccount,
-  useChainId,
-  usePublicClient,
-  useReadContract,
-  useWalletClient,
-} from "wagmi"
-import { SupportedChainId, morphoAddressesByChain } from "~/constants"
+  Address,
+  encodeFunctionData,
+  erc20Abi,
+  erc4626Abi,
+  maxUint256,
+  parseEther,
+} from "viem"
 
 export function useTestPosition() {
   const chainId = useChainId()
 
   const market = getAppConfig(chainId as SupportedChainId).morphoMarkets.at(1)
 
+  console.log(market)
+
   const { data: walletClient } = useWalletClient()
   const publicClient = usePublicClient()
 
-  const { address: destination } = useAccount()
+  // const { address: destination } = useAccount()
 
-  const { data: maxMintAmountLoan } = useReadContract({
-    abi: [
-      {
-        type: "function",
-        name: "maxMintAmount",
-        inputs: [],
-        outputs: [{ name: "", type: "uint256", internalType: "uint256" }],
-        stateMutability: "view",
-      },
-    ],
-    functionName: "maxMintAmount",
-    address: market?.loanToken.address,
-  })
+  const destination = "0x005182C62DA59Ff202D53d6E42Cef6585eBF9617" as Address
+
+  // const { data: maxMintAmountLoan } = useReadContract({
+  //   abi: [
+  //     {
+  //       type: "function",
+  //       name: "maxMintAmount",
+  //       inputs: [],
+  //       outputs: [{ name: "", type: "uint256", internalType: "uint256" }],
+  //       stateMutability: "view",
+  //     },
+  //   ],
+  //   functionName: "maxMintAmount",
+  //   address: market?.loanToken.address,
+  // })
 
   // For some reason the max mint amount for the collateral token is off
-  const maxMintAmountCollateral = 5000000000000000000000n
+  // const maxMintAmountCollateral = 5000000000000000000000n
 
   //   const { data: maxMintAmountCollateral } = useReadContract({
   //     abi: [
@@ -54,85 +60,162 @@ export function useTestPosition() {
   //   })
 
   return useMutation({
-    mutationKey: [
-      market?.hyperdrive,
-      "test-position",
-      maxMintAmountLoan?.toString(),
-      maxMintAmountCollateral?.toString(),
-    ],
+    mutationKey: [market?.hyperdrive, "test-position"],
     mutationFn: async () => {
-      if (
-        !destination ||
-        !maxMintAmountLoan ||
-        !maxMintAmountCollateral ||
-        !market ||
-        !walletClient ||
-        !publicClient ||
-        !market
-      )
+      if (!destination || !market || !walletClient || !publicClient || !market)
         return
 
-      const chainId = walletClient.chain.id
-      const morphoBlueAddress =
-        morphoAddressesByChain[chainId as SupportedChainId].blue
+      // const chainId = walletClient.chain.id
+      // const morphoBlueAddress =
+      //   morphoAddressesByChain[chainId as SupportedChainId].blue
 
-      // Math.min but for bigints
-      const maxMintAmount =
-        maxMintAmountLoan > maxMintAmountCollateral
-          ? maxMintAmountCollateral
-          : maxMintAmountLoan
+      // // Math.min but for bigints
+      // const maxMintAmount =
+      //   maxMintAmountLoan > maxMintAmountCollateral
+      //     ? maxMintAmountCollateral
+      //     : maxMintAmountLoan
 
-      let hash = await walletClient.writeContract({
-        abi: parseAbi([
-          "function mint(address destination, uint256 mintAmount)",
-        ]),
-        address: market.loanToken.address,
-        functionName: "mint",
-        args: [destination, maxMintAmountLoan],
+      await testClient.setBalance({
+        address: destination,
+        value: parseEther("100"),
       })
 
-      await publicClient.waitForTransactionReceipt({
-        hash,
-      })
+      console.log("Minted ETH to account")
 
-      hash = await walletClient.writeContract({
-        abi: parseAbi([
-          "function mint(address destination, uint256 mintAmount)",
-        ]),
-        address: market.collateralToken.address,
-        functionName: "mint",
-        args: [destination, maxMintAmountCollateral],
-      })
+      const USDE_WHALE_MAINNET =
+        "0xf89d7b9c864f589bbf53a82105107622b35eaa40" as Address
+      const USDE_ADDRESS_MAINNET =
+        "0x4c9EDD5852cd905f086C759E8383e09bff1E68B3" as Address
 
-      await publicClient.waitForTransactionReceipt({
-        hash,
-      })
+      const SUSDE_ADDRESS_MAINNET =
+        "0x9D39A5DE30e57443BfF2A8307A4256c8797A3497" as Address
 
-      hash = await walletClient.writeContract({
+      const DAI_ADDRESS_MAINNET =
+        "0x6b175474e89094c44da98b954eedeac495271d0f" as Address
+
+      // USDe
+      let transferData = encodeFunctionData({
         abi: erc20Abi,
-        address: market.loanToken.address,
-        functionName: "approve",
-        args: [morphoBlueAddress, maxUint256],
+        functionName: "transfer",
+        args: [destination as Address, parseEther("20000")],
       })
 
-      await publicClient.waitForTransactionReceipt({
-        hash,
+      let hash = await testClient.sendUnsignedTransaction({
+        from: USDE_WHALE_MAINNET,
+        to: USDE_ADDRESS_MAINNET,
+        data: transferData,
       })
 
-      hash = await walletClient.writeContract({
+      let receipt = await publicClient.waitForTransactionReceipt({ hash })
+
+      console.log("Transferred 20,000 USDe from whale: ", receipt)
+
+      // sUSDe
+
+      // Approve USDe spend
+      transferData = encodeFunctionData({
         abi: erc20Abi,
-        address: market.collateralToken.address,
         functionName: "approve",
-        args: [morphoBlueAddress, maxUint256],
+        args: [SUSDE_ADDRESS_MAINNET, maxUint256],
       })
 
-      await publicClient.waitForTransactionReceipt({
-        hash,
+      hash = await testClient.sendUnsignedTransaction({
+        from: destination,
+        to: USDE_ADDRESS_MAINNET,
+        data: transferData,
       })
 
-      hash = await walletClient.writeContract({
+      receipt = await publicClient.waitForTransactionReceipt({ hash })
+
+      console.log("Approved USDe spend for sUSDe deposit: ", receipt)
+
+      transferData = encodeFunctionData({
+        abi: erc4626Abi,
+        functionName: "deposit",
+        args: [parseEther("10000"), destination],
+      })
+
+      hash = await testClient.sendUnsignedTransaction({
+        from: destination,
+        to: SUSDE_ADDRESS_MAINNET,
+        data: transferData,
+      })
+
+      receipt = await publicClient.waitForTransactionReceipt({ hash })
+
+      console.log("Deposited 10,000 USDe for sUSDe: ", receipt)
+
+      // DAI
+      transferData = encodeFunctionData({
+        abi: erc20Abi,
+        functionName: "transfer",
+        args: [destination as Address, parseEther("20000")],
+      })
+
+      hash = await testClient.sendUnsignedTransaction({
+        from: "0x40ec5b33f54e0e8a33a975908c5ba1c14e5bbbdf",
+        to: DAI_ADDRESS_MAINNET,
+        data: transferData,
+      })
+
+      receipt = await publicClient.waitForTransactionReceipt({ hash })
+
+      console.log("Transferred 20,000 DAI from whale: ", receipt)
+
+      // wstETH
+      transferData = encodeFunctionData({
+        abi: erc20Abi,
+        functionName: "transfer",
+        args: [destination as Address, parseEther("10")],
+      })
+
+      hash = await testClient.sendUnsignedTransaction({
+        from: "0xc329400492c6ff2438472d4651ad17389fcb843a",
+        to: "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0",
+        data: transferData,
+      })
+      receipt = await publicClient.waitForTransactionReceipt({ hash })
+
+      console.log("Transferred 10 wstETH from whale: ", receipt)
+
+      // WETH
+      transferData = encodeFunctionData({
+        abi: WETHAbi,
+        functionName: "deposit",
+      })
+
+      hash = await testClient.sendUnsignedTransaction({
+        from: destination,
+        to: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        data: transferData,
+        value: parseEther("10"),
+      })
+
+      receipt = await publicClient.waitForTransactionReceipt({ hash })
+
+      console.log("Deposited 10 ETH for WETH: ", receipt)
+
+      // Supply Collateral to USDe/DAI market
+
+      // Approve USDe spend
+      transferData = encodeFunctionData({
+        abi: erc20Abi,
+        functionName: "approve",
+        args: [
+          morphoAddressesByChain[chainId as SupportedChainId].blue,
+          maxUint256,
+        ],
+      })
+      hash = await testClient.sendUnsignedTransaction({
+        from: destination,
+        to: USDE_ADDRESS_MAINNET,
+        data: transferData,
+      })
+      receipt = await publicClient.waitForTransactionReceipt({ hash })
+      console.log("Approved USDe spend for sUSDe collateral deposit: ", receipt)
+
+      transferData = encodeFunctionData({
         abi: MorphoBlueAbi,
-        address: morphoBlueAddress,
         functionName: "supplyCollateral",
         args: [
           {
@@ -142,19 +225,22 @@ export function useTestPosition() {
             collateralToken: market.collateralToken.address,
             loanToken: market.loanToken.address,
           },
-          maxMintAmount,
+          parseEther("10000"),
           destination,
           "0x",
         ],
       })
-
-      await publicClient.waitForTransactionReceipt({
-        hash,
+      hash = await testClient.sendUnsignedTransaction({
+        from: destination,
+        to: morphoAddressesByChain[chainId as SupportedChainId].blue,
+        data: transferData,
       })
+      receipt = await publicClient.waitForTransactionReceipt({ hash })
+      console.log("Deposited 10,000 USDe for collateral: ", receipt)
 
-      hash = await walletClient.writeContract({
+      // Borrow DAI
+      transferData = encodeFunctionData({
         abi: MorphoBlueAbi,
-        address: morphoBlueAddress,
         functionName: "borrow",
         args: [
           {
@@ -164,16 +250,329 @@ export function useTestPosition() {
             collateralToken: market.collateralToken.address,
             loanToken: market.loanToken.address,
           },
-          fixed(maxMintAmount).mul(85, 2).bigint,
+          parseEther("9000"),
           0n,
           destination,
           destination,
         ],
       })
-
-      await publicClient.waitForTransactionReceipt({
-        hash,
+      hash = await testClient.sendUnsignedTransaction({
+        from: destination,
+        to: morphoAddressesByChain[chainId as SupportedChainId].blue,
+        data: transferData,
       })
+      receipt = await publicClient.waitForTransactionReceipt({ hash })
+      console.log("Borrow 9,000 DAI from market: ", receipt)
+
+      return
+
+      // console.log(
+      //   await publicClient.readContract({
+      //     abi: ERC20MintableAbi,
+      //     address: market.loanToken.address,
+      //     functionName: "maxMintAmount",
+      //   })
+      // )
+
+      // console.log(
+      //   await publicClient.readContract({
+      //     abi: ERC20MintableAbi,
+      //     address: market.collateralToken.address,
+      //     functionName: "maxMintAmount",
+      //   })
+      // )
+
+      // const [maxLoanTokenMint, maxCollateralTokenMint] = await Promise.all([
+      //   publicClient.readContract({
+      //     abi: ERC20MintableAbi,
+      //     address: market.loanToken.address,
+      //     functionName: "maxMintAmount",
+      //   }),
+      //   publicClient.readContract({
+      //     abi: ERC20MintableAbi,
+      //     address: market.collateralToken.address,
+      //     functionName: "maxMintAmount",
+      //   }),
+      // ])
+
+      // console.log(maxLoanTokenMint, maxCollateralTokenMint)
+
+      // await walletClient.writeContract({
+      //   abi: MulticallAbi,
+      //   address: "0xcA11bde05977b3631167028862bE2a173976CA11",
+      //   functionName: "aggregate",
+      //   args: [
+      //     [
+      //       {
+      //         target: market.loanToken.address,
+      //         callData: encodeFunctionData({
+      //           abi: parseAbi([
+      //             "function mint(address destination, uint256 mintAmount)",
+      //           ]),
+      //           functionName: "mint",
+      //           args: [destination, maxMintAmountLoan],
+      //         }),
+      //       },
+      //       {
+      //         target: market.collateralToken.address,
+      //         callData: encodeFunctionData({
+      //           abi: parseAbi([
+      //             "function mint(address destination, uint256 mintAmount)",
+      //           ]),
+      //           functionName: "mint",
+      //           args: [destination, maxMintAmountLoan],
+      //         }),
+      //       },
+      //     ],
+      //   ],
+      // })
+
+      // let hash = await walletClient.writeContract({
+      //   abi: parseAbi([
+      //     "function mint(address destination, uint256 mintAmount)",
+      //   ]),
+      //   address: market.loanToken.address,
+      //   functionName: "mint",
+      //   args: [destination, maxMintAmountLoan],
+      // })
+
+      // await publicClient.waitForTransactionReceipt({
+      //   hash,
+      // })
+
+      // hash = await walletClient.writeContract({
+      //   abi: parseAbi([
+      //     "function mint(address destination, uint256 mintAmount)",
+      //   ]),
+      //   address: market.collateralToken.address,
+      //   functionName: "mint",
+      //   args: [destination, maxMintAmountCollateral],
+      // })
+
+      // await publicClient.waitForTransactionReceipt({
+      //   hash,
+      // })
+
+      // hash = await walletClient.writeContract({
+      //   abi: erc20Abi,
+      //   address: market.loanToken.address,
+      //   functionName: "approve",
+      //   args: [morphoBlueAddress, maxUint256],
+      // })
+
+      // await publicClient.waitForTransactionReceipt({
+      //   hash,
+      // })
+
+      // hash = await walletClient.writeContract({
+      //   abi: erc20Abi,
+      //   address: market.collateralToken.address,
+      //   functionName: "approve",
+      //   args: [morphoBlueAddress, maxUint256],
+      // })
+
+      // await publicClient.waitForTransactionReceipt({
+      //   hash,
+      // })
+
+      // hash = await walletClient.writeContract({
+      //   abi: MorphoBlueAbi,
+      //   address: morphoBlueAddress,
+      //   functionName: "supplyCollateral",
+      // args: [
+      //   {
+      //     irm: market.metadata.irm,
+      //     oracle: market.metadata.oracle,
+      //     lltv: market.lltv,
+      //     collateralToken: market.collateralToken.address,
+      //     loanToken: market.loanToken.address,
+      //   },
+      //   maxMintAmount,
+      //   destination,
+      //   "0x",
+      // ],
+      // })
+
+      // await publicClient.waitForTransactionReceipt({
+      //   hash,
+      // })
+
+      // hash = await walletClient.writeContract({
+      //   abi: MorphoBlueAbi,
+      //   address: morphoBlueAddress,
+      //   functionName: "borrow",
+      //   args: [
+      //     {
+      //       irm: market.metadata.irm,
+      //       oracle: market.metadata.oracle,
+      //       lltv: market.lltv,
+      //       collateralToken: market.collateralToken.address,
+      //       loanToken: market.loanToken.address,
+      //     },
+      //     fixed(maxMintAmount).mul(85, 2).bigint,
+      //     0n,
+      //     destination,
+      //     destination,
+      //   ],
+      // })
+
+      // await publicClient.waitForTransactionReceipt({
+      //   hash,
+      // })
     },
   })
 }
+
+const WETHAbi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "name",
+    outputs: [{ name: "", type: "string" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { name: "guy", type: "address" },
+      { name: "wad", type: "uint256" },
+    ],
+    name: "approve",
+    outputs: [{ name: "", type: "bool" }],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "totalSupply",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { name: "src", type: "address" },
+      { name: "dst", type: "address" },
+      { name: "wad", type: "uint256" },
+    ],
+    name: "transferFrom",
+    outputs: [{ name: "", type: "bool" }],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [{ name: "wad", type: "uint256" }],
+    name: "withdraw",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "decimals",
+    outputs: [{ name: "", type: "uint8" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "symbol",
+    outputs: [{ name: "", type: "string" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { name: "dst", type: "address" },
+      { name: "wad", type: "uint256" },
+    ],
+    name: "transfer",
+    outputs: [{ name: "", type: "bool" }],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "deposit",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [
+      { name: "", type: "address" },
+      { name: "", type: "address" },
+    ],
+    name: "allowance",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  { payable: true, stateMutability: "payable", type: "fallback" },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, name: "src", type: "address" },
+      { indexed: true, name: "guy", type: "address" },
+      { indexed: false, name: "wad", type: "uint256" },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, name: "src", type: "address" },
+      { indexed: true, name: "dst", type: "address" },
+      { indexed: false, name: "wad", type: "uint256" },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, name: "dst", type: "address" },
+      { indexed: false, name: "wad", type: "uint256" },
+    ],
+    name: "Deposit",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, name: "src", type: "address" },
+      { indexed: false, name: "wad", type: "uint256" },
+    ],
+    name: "Withdrawal",
+    type: "event",
+  },
+] as const
