20ca5dfbabcb68d5118652f150bb8b7e4b34cc40
Cash Baller <cashd@users.noreply.github.com>
2024-10-15 02:36:59 -0500

    update global fade animation

--- apps/ui/tailwind.config.js
+++ apps/ui/tailwind.config.js
@@ -124,7 +124,7 @@ module.exports = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        fade: "fadeIn .3s ease-in-out",
+        fade: "fadeIn .3s ease-in",
         fadeFast: "fadeIn .1s ease-in",
       },
     },
