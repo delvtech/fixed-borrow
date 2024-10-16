b8941c3c89944e593545e4e0b7d8e30a7bc44d29
Cash Baller <cashd@users.noreply.github.com>
2024-10-16 13:44:54 -0500

    Progress bar multi track update

--- apps/ui/src/components/base/progress.tsx
+++ apps/ui/src/components/base/progress.tsx
@@ -1,26 +1,48 @@
 import * as ProgressPrimitive from "@radix-ui/react-progress"
+import { cn } from "components/utils"
 import * as React from "react"
 
-import { cn } from "src/components/utils"
+type ProgressSegment = {
+  value: number
+  color?: string
+}
+
+type Props = React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & {
+  segments: ProgressSegment[]
+}
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
-  <ProgressPrimitive.Root
-    ref={ref}
-    className={cn(
-      "relative h-4 w-full overflow-hidden rounded-full bg-background",
-      className
-    )}
-    {...props}
-  >
-    <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-skyBlue transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
-    />
-  </ProgressPrimitive.Root>
-))
-Progress.displayName = ProgressPrimitive.Root.displayName
+  Props
+>(({ className, segments, ...props }, ref) => {
+  const sortedSegments = segments.sort((a, b) => a.value - b.value)
+
+  return (
+    <ProgressPrimitive.Root
+      ref={ref}
+      className={cn(
+        "relative h-6 w-full overflow-hidden rounded bg-secondary",
+        className
+      )}
+      {...props}
+    >
+      {sortedSegments.map((segment, index) => (
+        <ProgressPrimitive.Indicator
+          key={index}
+          className={cn(
+            "absolute h-full transition-all",
+            segment.color ? segment.color : "bg-primary"
+          )}
+          style={{
+            width: `${segment.value}%`,
+            zIndex: sortedSegments.length - index,
+          }}
+        />
+      ))}
+    </ProgressPrimitive.Root>
+  )
+})
+
+Progress.displayName = "Progress"
 
 export { Progress }
