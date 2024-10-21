bccfe41cd8152f493737727e81c5395bfb74f310
Cash Baller <cashd@users.noreply.github.com>
2024-10-21 15:48:42 -0500

    Update RestrictedCountriesPage.tsx

--- apps/ui/src/pages/RestrictedCountriesPage.tsx
+++ apps/ui/src/pages/RestrictedCountriesPage.tsx
@@ -4,7 +4,7 @@ import { ExternalLink, TriangleAlert } from "lucide-react"
 export function RestrictedCountriesPage() {
   return (
     <main>
-      <div className="flex w-screen flex-col items-center justify-center gap-8">
+      <div className="flex w-screen flex-col items-center justify-center gap-8 text-center">
         <div className="space-y-3">
           <h2 className="flex items-center justify-center gap-4">
             <TriangleAlert className="size-10 text-red-500" /> Restricted
